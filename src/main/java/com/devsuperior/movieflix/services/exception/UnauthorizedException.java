6935d8455532a8ef344043d4cc4be6694b06114c
Luiz Felipe <lffm1994@gmail.com>
2023-10-07 00:53:38 -0300

    Implementacao de Usuario

---
+++ src/main/java/com/devsuperior/movieflix/services/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.devsuperior.movieflix.services.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String msg) {
+        super(msg);
+    }
+}
